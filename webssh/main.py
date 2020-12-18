43c2c561f690585128d418055ae00d83e62bf8a9
Adam Raźniewski <adam@razniewski.eu>
2020-12-18 19:26:59 +0100

    Update main.py

--- webssh/main.py
+++ webssh/main.py
@@ -51,7 +51,6 @@ def main():
     if ssl_ctx:
         server_settings.update(ssl_options=ssl_ctx)
         app_listen(app, options.sslport, options.ssladdress, server_settings)
-    loop.start()
 
 
 if __name__ == '__main__':
